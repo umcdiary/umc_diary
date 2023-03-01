534fe9f9932ee2b473307aae4754a7458f342e6b
BaeGayeon <ebgy918@gmail.com>
2023-03-01 23:56:08 +0900

    chore: 회원 탈퇴하기 uri 수정

--- user/userRouter.js
+++ user/userRouter.js
@@ -5,7 +5,7 @@ import { patchNickname, deleteUser, getUserInfo } from './userController';
 const userRouter = express.Router();
 
 userRouter.patch('/nickname/:userId', patchNickname);
-userRouter.delete('/delete/:userId', deleteUser);
+userRouter.delete('/:userId', deleteUser);
 userRouter.get('/info/:userId', getUserInfo);
 
 export default userRouter;
